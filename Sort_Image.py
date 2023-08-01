fda66d9ff9d5abeeb1ed1a70a40fbc0674b6d55d
Marc-Antoine <140739460+marc04ant@users.noreply.github.com>
2023-07-31 17:06:27 -0700

    Update and rename real.py to Sort_Image.py

--- Sort_Image.py
+++ Sort_Image.py
@@ -1,6 +1,6 @@
 import os       #I used os package to allow python to work with operating system
-from tkinter import Tk, Label, Button, Canvas, filedialog # tkinter is to create the graphic interface
-from PIL import Image, ImageTk, ImageOps # Pillow package used for image processing and display
+from tkinter import Tk, Label, Button, Canvas, filedialog # tkinter is used to create the graphic interface
+from PIL import Image, ImageTk, ImageOps # Pillow package is used for image processing and display
 
 class ImageViewer: # We create this class to handle the main window and allow displaying image
     def __init__(self, root, image_paths, destination_folder): # The __init__ method initialize the imageViewer with providing, image path, destination folder and tkinter window
@@ -24,27 +24,27 @@ class ImageViewer: # We create this class to handle the main window and allow di
         self.deselect_button = Button(root, text="deselect", command=self.deselect_image, state="disabled") # To create the deselct button
         self.deselect_button.pack()
 
-        # Canva section where we create the small green circle.
+         # Canva section where we create the small green circle.
         self.canvas = Canvas(root, width=30, height=30)
         self.canvas.create_oval(10, 10, 20, 20, fill="green", state="hidden")
         self.canvas.pack()
 
         self.show_current_image()
 
-    def show_current_image(self): # This function display the image at a specific index
-        image_path = self.image_paths[self.current_image_index] # We define which image to display
+    def show_current_image(self): # This function display the image at a specific index.
+        image_path = self.image_paths[self.current_image_index] # We define which image will display.
         image = Image.open(image_path) # To open the image
         image = ImageOps.exif_transpose(image)  # Manage orientation issues
         image = image.resize((400, 400))  # resize image
-        photo = ImageTk.PhotoImage(image) # To create a compatible object between tkinter and Pillow package. We convert photo into a Tkinter PHotoImage
-        self.image_label.config(image=photo) # To set the property of image to the label
+        photo = ImageTk.PhotoImage(image) # To create a compatible object between tkinter and Pillow package. We convert photo into a Tkinter PHotoImage.
+        self.image_label.config(image=photo) # To set the property of image to the label.
         self.image_label.image = photo # Here we update the image attribute of the label to the tkinter PhotoImage.
 
         # update of the green symbol.
         if self.is_image_selected(image_path):
-            self.canvas.itemconfig(1, state="normal") # to display the small green circle if image is selected
+            self.canvas.itemconfig(1, state="normal") # to display the small green circle if image is selected.
         else:
-            self.canvas.itemconfig(1, state="hidden") # to hide the small green circle if image isn't selected
+            self.canvas.itemconfig(1, state="hidden") # to hide the small green circle if image isn't selected.
 
     def show_next_image(self): # we define what function will do the next button
         self.current_image_index = (self.current_image_index + 1) % len(self.image_paths) #To navigate between the different images we use the modulo methode which is really useful because when we reach the last index it restart from the begining. Here we add 1 to the current index to go to the next image.
@@ -56,7 +56,7 @@ class ImageViewer: # We create this class to handle the main window and allow di
         self.show_current_image()
         self.update_deselect_button_state()
 
-    def select_image(self): # This function copy the current displayed image into an other folder where we store selected images.
+    def select_image(self): # This function copies the current displayed image into an other folder where we store the selected images.
         image_path = self.image_paths[self.current_image_index] # We set the full path of the current image
         self.copy_image_to_specific_folder(image_path)
         self.show_current_image()
@@ -64,14 +64,14 @@ class ImageViewer: # We create this class to handle the main window and allow di
 
     def deselect_image(self):
         image_path = self.image_paths[self.current_image_index]
-        image_name = os.path.basename(image_path) # here we define just the name of the folder
-        destination_path = os.path.join(self.destination_folder, image_name) # here we add the name of the folder to full destination path like that we get the full path of the copied folder.
+        image_name = os.path.basename(image_path) # here we just define the name of the folder
+        destination_path = os.path.join(self.destination_folder, image_name) # here we add the name of the folder to full destination path. Like that we get the full path of the copied folder.
         try:
-            os.remove(destination_path) # to remove the image from the storage folder
-            print(f"Image '{image_name}' deselected and deleted of the directory : {self.destination_folder}") # To insure the operation worked well
+            os.remove(destination_path) # to remove the image from the storage folder.
+            print(f"Image '{image_name}' deselected and deleted of the directory : {self.destination_folder}") # To insure the operation worked well.
             self.show_current_image()
             self.update_deselect_button_state()
-        except FileNotFoundError: #if there is not such a file in the storage directory we display this message.
+        except FileNotFoundError: #if there isn't such a file in the storage directory we display this message.
             print(f"Impossible to deselect '{image_name}'. Image doesn't exist in the directory : {self.destination_folder}")
 
     def copy_image_to_specific_folder(self, image_path):
